8fddeced25b44dab152b08be945b2db8b5ea4b7a
Daniel Dyla <dyladan@users.noreply.github.com>
2021-07-14 09:25:10 -0400

    Exponential backoff and testing (#8)

--- spec/outputs/dynatrace_spec.rb
+++ spec/outputs/dynatrace_spec.rb
@@ -19,133 +19,109 @@ require 'logstash/codecs/plain'
 require 'logstash/event'
 require 'sinatra'
 require 'insist'
-
-PORT = rand(65_535 - 1024) + 1025
-
-# NOTE: that Sinatra startup and shutdown messages are directly logged to stderr so
-# it is not really possible to disable them without reopening stderr which is not advisable.
-#
-# == Sinatra (v1.4.6) has taken the stage on 51572 for development with backup from WEBrick
-# == Sinatra has ended his set (crowd applauds)
-#
-class TestApp < Sinatra::Base
-  # disable WEBrick logging
-  def self.server_settings
-    { AccessLog: [], Logger: WEBrick::BasicLog.new(nil, WEBrick::BasicLog::FATAL) }
-  end
-
-  class << self
-    attr_accessor :last_request
-
-    def clear
-      self.last_request = nil
-    end
-  end
-
-  post '/good' do
-    self.class.last_request = request
-    [204, '']
-  end
-
-  post '/bad' do
-    self.class.last_request = request
-    [400, 'Bad']
-  end
-end
-
-RSpec.configure do |config|
-  # http://stackoverflow.com/questions/6557079/start-and-call-ruby-http-server-in-the-same-script
-  def sinatra_run_wait(app, opts)
-    queue = Queue.new
-
-    t = java.lang.Thread.new(
-      proc do
-        begin
-          app.run!(opts) do |_server|
-            queue.push('started')
-          end
-        rescue StandardError => e
-          puts "Error in webserver thread #{e}"
-          # ignore
-        end
-      end
-    )
-    t.daemon = true
-    t.start
-    queue.pop # blocks until the run! callback runs
-  end
-
-  config.before(:suite) do
-    sinatra_run_wait(TestApp, port: PORT, server: 'webrick')
-    puts "Test webserver on port #{PORT}"
-  end
-end
+require 'net/http'
+require 'json'
 
 describe LogStash::Outputs::Dynatrace do
-  let(:port) { PORT }
-  let(:event) do
-    LogStash::Event.new({ 'message' => 'hi' })
+  let(:events) do
+    [
+      LogStash::Event.new({ 'message' => 'message 1', '@timestamp' => "2021-06-25T15:46:45.693Z" }),
+      LogStash::Event.new({ 'message' => 'message 2', '@timestamp' => "2021-06-25T15:46:46.693Z" }),
+    ]
   end
-  let(:url) { "http://localhost:#{port}/good" }
+  let(:url) { "http://localhost/good" }
   let(:key) { 'api.key' }
   let(:subject) { LogStash::Outputs::Dynatrace.new({ 'api_key' => key, 'ingest_endpoint_url' => url }) }
 
   before do
     subject.register
-    allow(subject).to receive(:log_failure).with(any_args)
+    subject.plugin_version = "1.2.3"
   end
 
-  context 'sending no events' do
-    it 'does not crash on empty events' do
-      subject.multi_receive([])
+  it 'does not send empty events' do
+    allow(subject).to receive(:send)
+    subject.multi_receive([])
+    expect(subject).to_not have_received(:send)
+  end
+
+  context 'server response success' do
+    it 'sends events' do
+      allow(subject).to receive(:send) do |req|
+        body = JSON.parse(req.body)
+        expect(body.length).to eql(2)
+        expect(body[0]['message']).to eql('message 1')
+        expect(body[0]['@timestamp']).to eql('2021-06-25T15:46:45.693Z')
+        expect(body[1]['message']).to eql('message 2')
+        expect(body[1]['@timestamp']).to eql('2021-06-25T15:46:46.693Z')
+        Net::HTTPOK.new "1.1", "200", "OK"
+      end
+      subject.multi_receive(events)
+      expect(subject).to have_received(:send)
+    end
+
+    it 'includes authorization header' do
+      allow(subject).to receive(:send) do |req|
+        expect(req['Authorization']).to eql("Api-Token #{key}")
+        Net::HTTPOK.new "1.1", "200", "OK"
+      end
+      subject.multi_receive(events)
+      expect(subject).to have_received(:send)
+    end
+
+    it 'includes content type header' do
+      allow(subject).to receive(:send) do |req|
+        expect(req['Content-Type']).to eql('application/json; charset=utf-8')
+        Net::HTTPOK.new "1.1", "200", "OK"
+      end
+      subject.multi_receive(events)
+      expect(subject).to have_received(:send)
+    end
+
+    it 'includes user agent' do
+      allow(subject).to receive(:send) do |req|
+        expect(req['User-Agent']).to eql('logstash-output-dynatrace v1.2.3')
+        Net::HTTPOK.new "1.1", "200", "OK"
+      end
+      subject.multi_receive(events)
+      expect(subject).to have_received(:send)
+    end
+
+    it 'does not log on success' do
+      allow(subject.logger).to receive(:debug) { raise "should not log" }
+      allow(subject.logger).to receive(:info) { raise "should not log" }
+      allow(subject.logger).to receive(:error) { raise "should not log" }
+      allow(subject.logger).to receive(:warn) { raise "should not log" }
+      allow(subject).to receive(:send) do |req|
+        Net::HTTPOK.new "1.1", "200", "OK"
+      end
+      subject.multi_receive(events)
+      expect(subject).to have_received(:send)
     end
   end
 
-  context 'with passing requests' do
-    before do
-      TestApp.clear
-      subject.multi_receive([event])
-    end
-
-    let(:last_request) { TestApp.last_request }
-    let(:body) { last_request.body.read }
-    let(:content_type) { last_request.env['CONTENT_TYPE'] }
-    let(:authorization) { last_request.env['HTTP_AUTHORIZATION'] }
-
-    let(:expected_body) { "#{LogStash::Json.dump([event])}\n" }
-    let(:expected_content_type) { 'application/json; charset=utf-8' }
-    let(:expected_authorization) { "Api-Token #{key}" }
-
-    it 'should not log a failure' do
-      expect(subject).not_to have_received(:log_failure).with(any_args)
-    end
-
-    it 'should receive the request' do
-      expect(last_request).to be_truthy
-    end
-
-    it 'should receive the event as a hash' do
-      expect(body).to eql(expected_body)
-    end
-
-    it 'should have the correct content type' do
-      expect(content_type).to eql(expected_content_type)
-    end
-
-    it 'should have the correct authorization' do
-      expect(authorization).to eql(expected_authorization)
+  context 'with bad client request' do
+    it 'does not retry on 404' do
+      allow(subject).to receive(:send) { Net::HTTPNotFound.new "1.1", "404", "Not Found" }
+      subject.multi_receive(events)
+      expect(subject).to have_received(:send).once
     end
   end
 
-  context 'with failing requests' do
-    let(:url) { "http://localhost:#{port}/bad" }
+  context 'with server error' do
+    it 'retries 5 times with exponential backoff' do
+      allow(subject).to receive(:sleep)
+      allow(subject).to receive(:send) { Net::HTTPInternalServerError.new "1.1", "500", "Internal Server Error" }
 
-    before do
-      subject.multi_receive([event])
-    end
+      subject.multi_receive(events)
 
-    it 'should log a failure' do
-      expect(subject).to have_received(:log_failure).with(any_args)
+      expect(subject).to have_received(:sleep).with(1).ordered
+      expect(subject).to have_received(:sleep).with(2).ordered
+      expect(subject).to have_received(:sleep).with(4).ordered
+      expect(subject).to have_received(:sleep).with(8).ordered
+      expect(subject).to have_received(:sleep).with(16).ordered
+
+      expect(subject).to have_received(:sleep).exactly(5).times
+      expect(subject).to have_received(:send).exactly(6).times
     end
   end
 end
