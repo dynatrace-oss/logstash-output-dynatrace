08178ebe6d2cf7273d760e83190375f86ff62d01
Daniel Dyla <dyladan@users.noreply.github.com>
2022-10-05 08:13:55 -0400

    Fix test wording

--- spec/outputs/dynatrace_spec.rb
+++ spec/outputs/dynatrace_spec.rb
@@ -137,7 +137,7 @@ describe LogStash::Outputs::Dynatrace do
       allow(subject).to receive(:send).and_return(response)
       subject.multi_receive(events)
 
-      expect(subject.logger).to have_received(:error).with("Encountered an HTTP client error in HTTP output",
+      expect(subject.logger).to have_received(:error).with("Encountered a client error in HTTP output",
         {:body=>"this is a failure", :code=>"400", :message=> "Client error"})
     end
   end
